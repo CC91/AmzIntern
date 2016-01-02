b7059e4d5af51aa48332741294bfefe80f20ef86
CC <caocancabbage@gmail.com>
2016-01-01 21:28:20 -0600

    WordBreak

---
+++ WordBreak.cpp
@@ -0,0 +1,17 @@
+// 139. Word Break
+class Solution {
+public:
+    bool wordBreak(string s, unordered_set<string>& wordDict) {
+        vector<bool> dp(s.size()+1, false);
+        dp[0] = true;
+        for (int i=0; i<s.size(); i++) {
+            for (int j=i; j>=0; j--) {
+                if (dp[j] && wordDict.count(s.substr(j, i-j+1))) {
+                    dp[i+1] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.size()];
+    }
+};
